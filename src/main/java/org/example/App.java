79de2640db00cb0b671d637477b2fefab757cf65
alina-Fattakhova <56797236+alina-Fattakhova@users.noreply.github.com>
2021-09-01 09:39:17 +0300

    deleted unneeded code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,10 +9,5 @@ import org.openqa.selenium.chrome.ChromeDriver;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-    }
+    public static void main( String[] args ) {}
 }
